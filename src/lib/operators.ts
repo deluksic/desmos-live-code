316971d1a2a6d099fb54f0fbe966841cee8e8bc3
David Emanuel Luksic <deluksic@gmail.com>
2025-12-02 21:04:13 +0100

    More operators

--- src/lib/operators.ts
+++ src/lib/operators.ts
@@ -1,19 +1,86 @@
+export type ExpressionOpts = Partial<
+  Omit<Desmos.ExpressionState & { type: "expression" }, "type" | "id">
+>;
+
 export function op(fnName: string, ...args: string[]) {
-  return `\\operatorname{${fnName}}(${args.join(",")})`;
+  return `\\operatorname{${fnName}}\\left(${args.join(",")}\\right)`;
 }
 
 export function point(
   identifier: string,
   definitionLatex: string,
-  opts?: Partial<
-    Omit<Desmos.ExpressionState & { type: "expression" }, "type" | "id">
-  >
+  opts?: ExpressionOpts
 ): Desmos.ExpressionState {
   return {
     type: "expression",
     id: `point-${identifier}`,
     label: identifier,
-    latex: `${identifier} = ${definitionLatex}`,
+    latex: `${identifier}=${definitionLatex}`,
+    showLabel: true,
     ...opts,
   };
+}
+
+export function vector(
+  identifier: string,
+  a: string,
+  b: string,
+  opts?: ExpressionOpts
+): Desmos.ExpressionState {
+  const definitionLatex = op("vector", a, b);
+  return {
+    type: "expression",
+    id: `vector-${identifier}`,
+    latex: identifier ? `${identifier}=${definitionLatex}` : definitionLatex,
+    hidden: true,
+    ...opts,
+  };
+}
+
+type SliderBounds = (Desmos.ExpressionState & {
+  type: "expression";
+})["sliderBounds"];
+
+export function slider(
+  identifier: string,
+  definitionLatex: number | string,
+  sliderBounds?: Partial<SliderBounds>,
+  opts?: ExpressionOpts
+): Desmos.ExpressionState {
+  return {
+    type: "expression",
+    id: `slider-${identifier}`,
+    latex: `${identifier}=${definitionLatex}`,
+    // @ts-expect-error
+    sliderBounds,
+    ...opts,
+  };
+}
+
+export function abs(value: string) {
+  return `\\left|${value}\\right|`;
+}
+
+export function div(a: string | number, b: string | number) {
+  return `\\frac{${a}}{${b}}`;
+}
+
+export function expr(
+  identifierOrDefinition: string,
+  latexDefinition?: string,
+  opts?: ExpressionOpts
+): Desmos.ExpressionState {
+  return {
+    type: "expression",
+    id: `expr-${identifierOrDefinition}`,
+    latex: latexDefinition
+      ? `${identifierOrDefinition}=${latexDefinition}`
+      : identifierOrDefinition,
+    hidden: true,
+    ...opts,
+  };
+}
+
+export function neg(value: string | number) {
+  return `-${value}`;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,62 @@
-import { op, point } from "./lib/operators";
+import {
+  abs,
+  div,
+  expr,
+  neg,
+  op,
+  point,
+  slider,
+  vector,
+  type ExpressionOpts,
+} from "./lib/operators";
 import { runExample } from "./lib/runExample";
 
-function* linesDemo(): Generator<Desmos.ExpressionState> {
-  yield point("P_1", "(0, 0)");
-  yield point("P_2", "(0, 5)");
-  yield {
-    id: "hello-world",
-    type: "expression",
-    latex: op("segment", "P_1", "P_2"),
-    color: "#000000",
-  };
+const guideLineStyle: ExpressionOpts = {
+  hidden: false,
+  lineStyle: "DASHED",
+  lineOpacity: 0.1,
+  color: "black",
+};
+
+function* externalNormals(A: string, B: string, rA: string, rB: string) {
+  const dAB = `d_{${A}${B}}`;
+  const nAB = `n_{${A}${B}}`;
+  const nLAB = `n_{L${A}${B}}`;
+  const nRAB = `n_{R${A}${B}}`;
+  const alpha = `\\alpha_{${A}${B}}`;
+  const v2 = `v_{${A}${B}2}`;
+  const v3 = `v_{${A}${B}3}`;
+  const v4 = `v_{${A}${B}4}`;
+  const v5 = `v_{${A}${B}5}`;
+  yield vector(dAB, A, B);
+  yield expr(nAB, div(dAB, abs(dAB)));
+  yield expr(alpha, op("arccos", div(`${rA}-${rB}`, op("length", dAB))));
+  yield expr(nLAB, op("rotate", nAB, A, alpha));
+  yield expr(nRAB, op("rotate", nAB, A, neg(alpha)));
+  yield expr(v2, op("translate", A, rA + nLAB));
+  yield expr(v3, op("translate", A, rA + nRAB));
+  yield expr(v4, op("translate", B, rB + nRAB));
+  yield expr(v5, op("translate", B, rB + nLAB));
+  yield expr(op("line", v2, v5), undefined, guideLineStyle);
+  yield expr(op("line", v3, v4), undefined, guideLineStyle);
 }
 
-runExample(linesDemo())
+function* linesDemo(): Generator<Desmos.ExpressionState> {
+  yield slider("r_a", 1, { min: 0, max: 5 });
+  yield slider("r_b", 2, { min: 0, max: 5 });
+  yield slider("r_c", 1, { min: 0, max: 5 });
+  yield slider("r_d", 1, { min: 0, max: 5 });
+  yield point("A", "(0, 0)");
+  yield point("B", "(2, 0)");
+  yield point("C", "(4, 0)");
+  yield point("D", "(6, 0)");
+  yield expr(op("circle", "A", "r_a"), undefined, guideLineStyle);
+  yield expr(op("circle", "B", "r_b"), undefined, guideLineStyle);
+  yield expr(op("circle", "C", "r_c"), undefined, guideLineStyle);
+  yield expr(op("circle", "D", "r_d"), undefined, guideLineStyle);
+  yield* externalNormals("A", "B", "r_a", "r_b");
+  yield* externalNormals("B", "C", "r_b", "r_c");
+  yield* externalNormals("C", "D", "r_c", "r_d");
+}
+
+runExample(linesDemo());
